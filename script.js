2092906a966aface4d41ccc191da6eecb33a7ed8
pallavigawande <95491010+pallavigawande@users.noreply.github.com>
2023-01-31 00:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
-
 function palindrome(str){
+ var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
 
+  /* reverse removeChar for comparison*/
+  var checkPalindrome = removeChar.split('').reverse().join('');
+
+  /* Check to see if str is a Palindrome*/
+   if(removeChar === checkPalindrome){
+     return true;
+   }else{
+     return false;
+   }
 }
+
+
 module.exports = palindrome
